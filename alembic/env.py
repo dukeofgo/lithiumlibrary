1b26ecfdf6c15a93c666a1b1a25010c3fb966d28
dukeofgo <formtag0@gmail.com>
2024-11-14 22:00:31 +0700

    config alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -14,12 +15,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# set sqlalchemy.url in alembic.ini to POSTGRES_DATABASE_URL
+config.set_section_option("alembic", "sqlalchemy.url", os.environ.get("POSTGRES_DATABASE_URL"))
+
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
-
+from src.database import Base
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
